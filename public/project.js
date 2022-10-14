d42ffaf4c86d4b9fd42a7c8566d7c51285b1bcaf
EugeneStone <89702595+EugeneStone@users.noreply.github.com>
2022-10-14 05:21:44 +0300

    version 1.0

---
+++ public/project.js
@@ -0,0 +1,16 @@
+let project = [
+    {
+        name:"portfolio website",
+        tags:"javascript",
+        image: "project1.png",
+        location: ""
+        
+    },
+    {
+        name:"todo-list",
+        tags:"javascript" "React",
+        image: "project1.png",
+        location: ""
+        
+    },
+]
